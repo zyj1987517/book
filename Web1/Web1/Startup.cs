e829b91f67fe889177928bfe9004dfef3d8be861
j <j@J-HP>
2016-07-31 10:50:19 +0800

    zhuyajie

---
+++ Web1/Web1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web1.Startup))]
+namespace Web1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
